582e0f13217198c0db72d9bbbc99b2c937003320
Krish <kpatel5341@stu.d214.org>
2023-01-23 10:41:50 -0600

    Initial Commit

---
+++ team-maddux2/team_maddux2App.swift
@@ -0,0 +1,17 @@
+//
+//  team_maddux2App.swift
+//  team-maddux2
+//
+//  Created by Krish Patel on 1/23/23.
+//
+
+import SwiftUI
+
+@main
+struct team_maddux2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
