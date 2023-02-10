13efc3c9712ddd0e99b226eac2763c60e92eb1af
refractored <fatboy11905@gmail.com>
2023-02-10 11:02:32 -0600

    Init navigate

--- team-maddux2/ContentView.swift
+++ team-maddux2/ContentView.swift
@@ -65,7 +65,31 @@ struct ContentView: View {
         }
     
 }
+extension View {
+    /// Navigate to a new view.
+    /// - Parameters:
+    ///   - view: View to navigate to.
+    ///   - binding: Only navigates when this condition is `true`.
+    func navigate<NewView: View>(to view: NewView, when binding: Binding<Bool>) -> some View {
+        NavigationView {
+            ZStack {
+                self
+                    .navigationBarTitle("")
+                    .navigationBarHidden(true)
 
+                NavigationLink(
+                    destination: view
+                        .navigationBarTitle("")
+                        .navigationBarHidden(true),
+                    isActive: binding
+                ) {
+                    EmptyView()
+                }
+            }
+        }
+        .navigationViewStyle(.stack)
+    }
+}
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
